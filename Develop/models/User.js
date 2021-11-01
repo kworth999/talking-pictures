de7982a6c9897bdd241403d9c6fb8880031a2fca
Johnsonha801 <84554237+Johnsonha801@users.noreply.github.com>
2021-11-01 17:43:01 -0600

    added more models

---
+++ Develop/models/User.js
@@ -0,0 +1,46 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+    checkPassword(pw) {
+        return bcrypt.compareSync(pw, this.password);
+    }
+}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    }, 
+    {
+        hooks: {
+            async beforeCreate(userData) {
+                userData.password = await bcrypt.hash(userData.password, 10);
+                return userData;
+            }
+        },
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
