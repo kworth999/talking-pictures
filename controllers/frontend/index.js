1c051e28474434db6b01d506fa28b1286623e493
Calvin Donner <calvinjdonner@gmail.com>
2021-11-04 18:51:54 -0600

    refactor / route

--- controllers/frontend/index.js
+++ controllers/frontend/index.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const dashboardRoutes = require('./dashboardRoutes');
 const homeRoutes = require('./homeRoutes');
 
-router.use('/dashboard', dashboardRoutes);
+router.use('/', dashboardRoutes);
 router.use('/homepage', homeRoutes);
 module.exports = router;
