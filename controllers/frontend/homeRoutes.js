8c457340cdd360da5ac9956241537b3d84ac2990
Calvin Donner <calvinjdonner@gmail.com>
2021-11-04 18:48:49 -0600

    refactor

--- controllers/frontend/homeRoutes.js
+++ controllers/frontend/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const sequelize = require('../../config/connection');
+const { Post, User } = require('../../models');
 
 router.get('/', (req, res) => {
     console.log('=====================');
@@ -15,7 +15,7 @@ router.get('/', (req, res) => {
     ],
     include: [
         {
-            model: User, 
+            model: User,
             attributes: ['username']
         }
     ]
@@ -33,3 +33,5 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+module.exports = router;
