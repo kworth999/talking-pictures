7ba5e3b549fea3db3f64d0789bbd55d6d52b9589
Calvin Donner <calvinjdonner@gmail.com>
2021-11-05 23:45:09 -0600

    models review changes

--- controllers/frontend/homeRoutes.js
+++ controllers/frontend/homeRoutes.js
@@ -33,4 +33,43 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/post/:id', (req, res) => {
+    Reviews.findOne({
+        where: { 
+            id: req.params.id
+        },
+        attributes: [ 'id', 'imdb_id', 'user_id', 'rating', 'comment' ],
+        include: [
+        {
+            model: User,
+            attributes: [ 'username' ]
+        }
+    ]
+    })
+    .then(dbReviewData => {
+        if (!dbReviewData) {
+            res.status(404).json({ message: 'No review found with this id' });
+            return;
+        }
+        const review = dbReviewData.get({ plain: true });
+
+        res.render('single-post', {
+            review,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
