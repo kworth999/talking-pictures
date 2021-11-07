81adb51237080e9a1680faa231e8937718d69e13
Calvin Donner <calvinjdonner@gmail.com>
2021-11-06 21:55:38 -0600

    updates to login.js fetch request

--- controllers/frontend/loginRoutes.js
+++ controllers/frontend/loginRoutes.js
@@ -1,25 +1,25 @@
-const router = require('express').Router();
+// const router = require('express').Router();
 
-// Login Page - /login
-router.get('/login', (req, res) => {
-    // const isUser = (req.session.role_id === 2) ? true : false;
-    res.render('login', { loggedIn: req.session.loggedIn, });
+// // Login Page - /login
+// router.get('/login', (req, res) => {
+//     // const isUser = (req.session.role_id === 2) ? true : false;
+//     res.render('login', { loggedIn: req.session.loggedIn, });
    
     
     
-});
+// });
 
-// Review Form Page - /review
-router.get('/review', (req, res) => {
-    const isUser = (req.session.role_id === 2) ? true : false;
-    res.render('Review', { loggedIn: req.session.loggedIn,});
-});
+// // Review Form Page - /review
+// router.get('/review', (req, res) => {
+//     const isUser = (req.session.role_id === 2) ? true : false;
+//     res.render('Review', { loggedIn: req.session.loggedIn,});
+// });
 
-// Review Form Page - /review
-router.get('/', (req, res) => {
-    // const isUser = (req.session.role_id === 2) ? true : false;
-    // res.render('Review', { loggedIn: req.session.loggedIn});
-    res.render('index', {});
-});
+// // Review Form Page - /review
+// router.get('/', (req, res) => {
+//     // const isUser = (req.session.role_id === 2) ? true : false;
+//     // res.render('Review', { loggedIn: req.session.loggedIn});
+//     res.render('index', {});
+// });
 
-module.exports = router;
+// module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -29,7 +29,7 @@ async function signupFormHandler(event) {
   const password = document.querySelector('#password-signup').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users', {
+    const response = await fetch('/api/user', {
       method: 'post',
       body: JSON.stringify({
         username,
@@ -56,7 +56,7 @@ async function loginFormHandler(event) {
   const password = document.querySelector('#password-login').value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/user/login', {
+    const response = await fetch('/api/user', {
       method: 'post',
       body: JSON.stringify({
         username,
