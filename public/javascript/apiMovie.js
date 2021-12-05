27bd41a43b27bc2789cd0f14b274060f3145f7c2
Calvin Donner <calvinjdonner@gmail.com>
2021-12-04 20:20:07 -0700

    updates to api call and post-info hb

--- public/javascript/apiMovie.js
+++ public/javascript/apiMovie.js
@@ -7,4 +7,13 @@ async function getMovies(searchText) {
     let response = await fetch('http://www.omdbapi.com/?apikey=2f2afafe&s='+searchText)
     let data = await response.json();
     console.log(data.Search);
-}
+    let movies = data.Search[0];
+    let output = '';
+    output += `
+    <div class="col-md-3">
+        <div class="well text-center">
+            <img src="${movies.Poster}">
+        </div>
+    </div>
+    `;
+};
